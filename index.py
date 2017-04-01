b3d1aeff3ea1837cf55087d3161026dec65dab29
khilstob <khilstob@gmail.com>
2017-03-31 23:57:20 -0700

    Get authors and genders

--- index.py
+++ index.py
@@ -1,22 +1,37 @@
 from goodreads import client
+import xml
 import requests
+import random
+import time
+from os import listdir
+import sys
+from bs4 import BeautifulSoup
 
-gc = client.GoodreadsClient("sySok63UyYyWi2Wof1HocA", "CyuRTp1MAMtnmF839U6653Uv8yn1Y0SWh0EF5d8ACNQ")
+def getAuthors ():
+  for i in range(0, 225):
+    randomAuthorId = str(random.randrange(1000, 7253283))
+    authorFile = open('./authors/' + randomAuthorId + '.xml', 'w')
+    result = requests.get('https://www.goodreads.com/author/show/' + randomAuthorId + '?format=xml&key=sySok63UyYyWi2Wof1HocA')
+    try:
+      authorFile.write(result.text)
+    except UnicodeEncodeError:
+      print 'One item skipped: UnicodeDecodeError'
+    print "Going to sleeep for 2 seconds."
+    time.sleep(2)
+    print "Awake!"
+    print randomAuthorId + " saved"
+    authorFile.close()
 
-print gc.book(1)
-print gc.book(1).authors[0]
+def getBooks():
+  authorFiles = listdir('./authors/')
+  for file in authorFiles:
+    soup = BeautifulSoup(open('./authors/' + file), 'xml')
+    genders = soup.findAll('gender')
+    if genders:
+      print genders[0]
 
+if sys.argv[1] == "getAuthors":
+  getAuthors()
 
-
-
-# import random
-# print (random.randrange(1, 1000))
-
-# import xml.etree.ElementTree as ET
-# tree = ET.parse('country_data.xml')
-# root = tree.getroot()
-# for i<1000:
-r = requests.get('https://www.goodreads.com/author/show/1234?format=xml&key=sySok63UyYyWi2Wof1HocA')
-print r.text
-
-
+if sys.argv[1] == "getBooks":
+  getBooks()
